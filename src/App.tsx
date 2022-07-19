6359ffa403dc6ff4ea6f48fb3d87641fa6daaf42
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-07-20 04:30:59 +0900

    feat: setup routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./css-reset.css";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import "./styles/themes.scss";
 import { useSelector } from "react-redux";
@@ -16,6 +16,17 @@ function App() {
       <Routes>
         <Route path="/" element={<Default />}>
           <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="new" element={<div>new</div>} />
+          <Route path="about" element={<div>about</div>} />
+          <Route path="contact" element={<div>contact</div>} />
+          <Route path="as" element={<Outlet />}>
+            <Route index element={<div>as (navigator here)</div>} />
+            <Route path="writer" element={<div>writer</div>} />
+            <Route path="filmmaker" element={<div>filmmaker</div>} />
+            <Route path="programmer" element={<div>programmer</div>} />
+            <Route path="student" element={<div>student</div>} />
+          </Route>
           <Route path="*" element={<Page404 />} />
         </Route>
       </Routes>
